99eced57c0afdc4df23cdd45b95bc52884a92eed
sai.chodapaneedi <sai.chodapaneedi@tigeranalytics.com>
2023-02-20 13:07:39 +0530

    Update main.tf
    dicount

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -108,29 +108,29 @@ resource   "azurerm_windows_virtual_machine"   "myvm123"   {
    } 
 }
 
-# resource "azurerm_windows_virtual_machine" "sai-vm" {
-#   name                = "sai-vm"
-#   resource_group_name = local.resource_group
-#   location            = local.location
-#   size                = "Standard_D2s_v3"
-#   admin_username      = var.admin_username
-#   admin_password      = var.admin_password
-#   network_interface_ids = [
-#     azurerm_network_interface.net02sai.id,
-#   ]
+resource "azurerm_windows_virtual_machine" "sai-vm" {
+  name                = "sai-vm"
+  resource_group_name = local.resource_group
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.net02sai.id,
+  ]
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-#   source_image_reference {
-#     publisher = "MicrosoftWindowsServer"
-#     offer     = "WindowsServer"
-#     sku       = "2019-Datacenter"
-#     version   = "latest"
-#   }
-# }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
 
 
 
