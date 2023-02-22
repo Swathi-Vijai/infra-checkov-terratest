a5a750dabfc0111f358f299adc2a7920010a0c71
Swathi.Vijai <swathi.vijai@tigeranalytics.com>
2023-02-23 01:04:01 +0530

    Update checkov.py

--- checkov.py
+++ checkov.py
@@ -24,6 +24,35 @@ def get_severity_value(url,check_id):
                               for x in l:
                                   if 'Severity' in x:
                                       return(x[10:1000])
+def format(pas):
+        lse=[]
+        lsc=[]
+        lsr=[]
+        for x in pas:
+            
+            sp=x.split(",")
+            lse.append(sp[0])
+            lsc.append(sp[1])
+            lsr.append(sp[2])
+        mc=0
+        for x in lsc:
+            if(len(x)) > mc:
+                mc=len(x)
+            
+        pas=[]
+        for i in range(len(lse)):
+            s=''
+            if len(lse[i]) != 10:
+                s=s + lse[i] + " "*(10-(len(lse[i])))
+            else:
+                s=s+lse[i]
+            if len(lsc[i]) != mc:
+                s=s + lsc[i] + " "*(mc-(len(lse[i])))
+            else:
+                s=s+lse[i]
+            s=s+ lsr[i]
+            pas.append(s)
+        return pas
 
 import json
 line = "-"*125
@@ -47,9 +76,9 @@ data=json.load(f)
 res = line + line_indicator
 if type(data) == list:
   for i in range(len(data)):
-      pas=[]
-      fail=[]
-      skip=[]
+      pas=["Severity,check-name,resource name "]
+      fail=["Severity,check-name,resource name "]
+      skip=["Severity,check-name,resource name "]
       res = res + str('Check Type:     ' +
                                                   data[i]["check_type"]).center(120)
       xdata = data[i]["summary"]
@@ -57,32 +86,35 @@ if type(data) == list:
       passed_check = (check_results["passed_checks"])
       for j in range(len((check_results["passed_checks"]))):
           sw=str(get_severity_value(passed_check[j]["guideline"], passed_check[j]["check_id"])) 
+          cn=(passed_check[j]["check_name"])
+          rn=(passed_check[j]["resource"])
           
           if sw in sl:
               skip.append(((str(sw)+ \
-                                                  " : " + (passed_check[j]["check_name"]) + " - Resource Name : " + (passed_check[j]["resource"]))))
+                                                  " , " + (passed_check[j]["check_name"]) + "," + (passed_check[j]["resource"]))))
           else:
               pas.append(((str(sw)+ \
-                                                  " : " + (passed_check[j]["check_name"]) + " - Resource Name : " + (passed_check[j]["resource"]))))
+                                                  " , " + (passed_check[j]["check_name"]) + " , " + (passed_check[j]["resource"]))))
           
       failed_check = (check_results["failed_checks"])
       for j in range(len((check_results["failed_checks"]))):
           sw=str(get_severity_value(failed_check[j]["guideline"], failed_check[j]["check_id"]))
           if sw in sl:
               skip.append(((str(sw)+ \
-                                                  " : " + (failed_check[j]["check_name"]) + " - Resource Name : " + (failed_check[j]["resource"]))))
+                                                  " , " + (failed_check[j]["check_name"]) + " , " + (failed_check[j]["resource"]))))
           else:
               if sw in Restricted_Severity:
                   if(hfc==0):
                       flag = 1
                       print(f"##vso[task.setvariable variable=FlagFailedSeverity;]{flag}")
               fail.append((str(sw)+ \
-                                                  " : " + (failed_check[j]["check_name"]) + " - Resource Name : " + (failed_check[j]["resource"])))
+                                                  " , " + (failed_check[j]["check_name"]) + " ," + (failed_check[j]["resource"])))
       skipped_checks = (check_results["skipped_checks"])
       for j in range(len((check_results["skipped_checks"]))):
           skip.append(((str(get_severity_value(skipped_checks[j]["guideline"], skipped_checks[j]["check_id"]))+ \
-                                                  " : " + (skipped_checks[j]["check_name"]) + " - Resource Name : " + (skipped_checks[j]["resource"]))))
+                                                  " ," + (skipped_checks[j]["check_name"]) + " ," + (skipped_checks[j]["resource"]))))
      
+      
       xdata['passed']=len(pas)
       xdata['failed'] = len(fail) 
       xdata['skipped']=len(skip)
@@ -91,6 +123,9 @@ if type(data) == list:
       o.write('\n')
       o.write(json.dumps(xdata))
       o.write('\n')
+      pas=format(pas)
+      fail=format(fail)
+      skip=format(skip)
      
       res = res + line_indicator + line + line_indicator + ' | '.join(xdata.keys()
                                                                                                   ) + line_indicator + divider + line_indicator + ' | '.join(map(str, xdata.values())) + line_indicator + line
